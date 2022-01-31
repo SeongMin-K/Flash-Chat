3ad17dcd2669095cba96a26348468e5a23e03bc6
SeongMin-K <eo1tks3@naver.com>
2022-01-31 15:35:44 +0900

    Firebase / Register / Log In / Log Out

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let er = error {
+                    print(er.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "roginToChatView", sender: self)
+                }
+            }
+        }
     }
-    
 }
