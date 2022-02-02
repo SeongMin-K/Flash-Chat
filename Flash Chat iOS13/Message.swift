a35b85cbdc42113a9178c6250b4972269827b250
SeongMin-K <eo1tks3@naver.com>
2022-02-02 16:19:26 +0900

    Custom UITableViewCell

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by SeongMinK on 2022/02/02.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
